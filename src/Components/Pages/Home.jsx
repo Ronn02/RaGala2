3f84a7b0ff0851389c5c2a506bb947d0146b1246
Ronn02 <72732177+Ronn02@users.noreply.github.com>
2023-07-25 14:33:43 +0800

    up

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -5,28 +5,28 @@ import Navbar from "../Navbar/Navbar";
 import RightSide from "../RightSidebar/RightSide";
 import Main from "../Main/Main";
 import RagalaNavbar from "../Navbar/RagalaNavbar";
+import PCNavBar from "../Navbar/PCNavBar";
 
 const Home = () => {
   return (
     <div className="w-full">
-      <div className="fixed top-0 z-10 w-full bg-white">
-        <Navbar></Navbar>
-      </div>
+      <PCNavBar></PCNavBar>
+      
       <div className="flex bg-gray-100">
-        <div className="flex-auto w-[20%] fixed top-12">
+        <div className="flex-auto w-[20%] fixed mt-[10px]">
           <LeftSide></LeftSide>
         </div>
-        <div className="flex-auto w-[60%] absolute left-[20%] top-14 bg-gray-100 rounded-xl">
+        <div className="flex-auto w-[60%] absolute left-[20%]  bg-gray-100 rounded-xl">
           <div className="w-[80%] mx-auto mt-[10px]">
            
             <Main></Main>
           </div>
         </div>
-        <div className="flex-auto w-[20%] fixed right-0 top-12">
+        <div className="flex-auto w-[20%] fixed right-0 mt-[10px]">
           <RightSide></RightSide>
         </div>
       </div>
-      <RagalaNavbar/>
+      
     </div>
   );
 };
