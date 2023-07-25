3b5ce141c7ba7806556e8c9153b01252ac4d3477
bea <beanyxjuanillas@gmail.com>
2023-07-25 18:03:47 +0800

    not in column pa po jebal hehe

--- src/Components/Pages/Province/Batangas/BatangasSpecific/PadrePioNationalShrine.jsx
+++ src/Components/Pages/Province/Batangas/BatangasSpecific/PadrePioNationalShrine.jsx
@@ -3,6 +3,7 @@ import RagalaNavbar from '../../../../Navbar/RagalaNavbar';
 import PadrePioNationalShrine1 from '../../../../../assets/destination/BATANGAS/PADRE PIO NATIONAL SHRINE/PADRE PIO NATIONAL SHRINE, BATANGAS - 1.jpg';
 import PadrePioNationalShrine2 from '../../../../../assets/destination/BATANGAS/PADRE PIO NATIONAL SHRINE/PADRE PIO NATIONAL SHRINE, BATANAGAS - 4.jpg';
 import PadrePioNationalShrine3 from '../../../../../assets/destination/BATANGAS/PADRE PIO NATIONAL SHRINE/PADRE PIO NATIONAL SHRINE, BATANGAS - 2.jpg';
+import PCNavBar from '../../../../Navbar/PCNavBar';
 
 const PadrePioNationalShrine = () => {
   const [currentImage, setCurrentImage] = useState(PadrePioNationalShrine1);
@@ -33,7 +34,8 @@ const PadrePioNationalShrine = () => {
 
   return (
     <div className='w-full min-h-screen'>
-      <div className='w-full flex justify-center items-center relative'>
+      <PCNavBar></PCNavBar>
+      <div className='w-full md:w-[50%] flex justify-center items-center relative'>
         {images.length > 1 && (
           <>
             <button
@@ -90,11 +92,11 @@ const PadrePioNationalShrine = () => {
           </div>
         )}
       </div>
-      <div className='flex flex-col items-center mt-4'>
-        <h1 className='text-5xl font-bold'>PadrePioNationalShrine</h1>
+      <div className='flex flex-col items-center md:mt-[20%] bg-green'>
+        <h1 className='text-5xl font-bold'>Padre Pio National Shrine</h1>
         <div className='flex mt-4 space-x-4'>
           <a
-            href='https://booking-site-url.com' // Replace with the actual booking site URL
+            href='https://www.klook.com/en-PH/hotels/detail/191573-bravo-tanauan-hotel/?check_in=2023-07-21&check_out=2023-07-22&room_num=1&adult_num=2&child_num=0&age=&page_source=hotel_list_page&server_data=81124443136e8db398c4150149cf9cb8abd8229ed0cc4b84f39ae1538c9297f9b055cbd5c038cc771b5d6a9b2963269a176e4c7be10db6b3257d24e8d3ced39c5f755303292cbe0deb82ad5b26d54ca7c40dfd9c44da728ef31f43dd57fe7804&source_price_token=eyJjdXJyZW5jeSI6IlBIUCIsInByaWNlIjo0NzAyLjgsInNvdXJjZSI6MX0%3D&is_suggest_time=1' // Replace with the actual booking site URL
             target='_blank'
             rel='noopener noreferrer'
             className='bg-blue-500 text-white px-4 py-2 rounded-md'
@@ -102,7 +104,7 @@ const PadrePioNationalShrine = () => {
             Book Now
           </a>
           <a
-            href='https://waze.com' // Replace with the actual Waze URL
+            href='https://www.waze.com/live-map/directions/parish-and-national-shrine-of-saint-padre-pio-santo-tomas-lipa-rd-santo-tomas?to=place.w.79429773.794166656.4070823' // Replace with the actual Waze URL
             target='_blank'
             rel='noopener noreferrer'
             className='bg-green-500 text-white px-4 py-2 rounded-md'
@@ -110,7 +112,9 @@ const PadrePioNationalShrine = () => {
             Get Directions (Waze)
           </a>
         </div>
-        <p className='mt-[30px]'>Put the short descriptions here</p> 
+        <div className='flex w-80 justify-center items-center mt-[30px]' /*added some css*/>
+            <p className='mx-3'>Batangas, Philippines, is blessed to be home to a significant religious landmark dedicated to one of the most beloved and revered saints in Catholicism – Saint Padre Pio. Located in the heart of Batangas City, the Padre Pio Shrine stands as a testament to faith, compassion, and miracles. A visit to this shrine offers a profound spiritual experience and a chance to learn about the life and legacy of this extraordinary saint.</p> 
+         </div>   
       </div>
       
       <RagalaNavbar />
