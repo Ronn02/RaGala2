763c69a9603281c8a7224215ad38a7b6f1a42c1b
bea <beanyxjuanillas@gmail.com>
2023-07-25 17:06:47 +0800

    updated fortune island

--- src/Components/Pages/Province/Batangas/BatangasSpecific/FortuneIsland.jsx
+++ src/Components/Pages/Province/Batangas/BatangasSpecific/FortuneIsland.jsx
@@ -3,6 +3,7 @@ import RagalaNavbar from '../../../../Navbar/RagalaNavbar';
 import FortuneIsland1 from '../../../../../assets/destination/BATANGAS/FORTUNE ISLAND/FORTUNE ISLAND, BATANGAS - 1.jpg';
 import FortuneIsland2 from '../../../../../assets/destination/BATANGAS/FORTUNE ISLAND/FORTUNE ISLAND, BATANGAS - 2.jpg';
 import FortuneIsland3 from '../../../../../assets/destination/BATANGAS/FORTUNE ISLAND/FORTUNE ISLAND, BATANGAS - 3.png';
+import PCNavBar from '../../../../Navbar/PCNavBar';
 
 const FortuneIsland = () => {
   const [currentImage, setCurrentImage] = useState(FortuneIsland1);
@@ -32,8 +33,9 @@ const FortuneIsland = () => {
   };
 
   return (
-    <div className='w-full min-h-screen'>
-      <div className='w-full flex justify-center items-center relative'>
+    <div className='w-full min-h-screen md:flex' /* additional css*/ >
+      <PCNavBar></PCNavBar>
+      <div className='w-full md:w-[50%] flex justify-center items-center relative'>
         {images.length > 1 && (
           <>
             <button
@@ -90,11 +92,11 @@ const FortuneIsland = () => {
           </div>
         )}
       </div>
-      <div className='flex flex-col items-center mt-4'>
-        <h1 className='text-5xl font-bold'>FortuneIsland</h1>
+      <div className='flex flex-col items-center justify-center mt-10 mx-3 md:w-[50%] md:mt-[20%] lg:mt-[10%]'/*changed */> 
+        <h1 className='text-5xl font-bold'>Fortune Island</h1>
         <div className='flex mt-4 space-x-4'>
           <a
-            href='https://booking-site-url.com' // Replace with the actual booking site URL
+            href='https://www.airbnb.com/fortune-island-philippines/stays' // booking site URL
             target='_blank'
             rel='noopener noreferrer'
             className='bg-blue-500 text-white px-4 py-2 rounded-md'
@@ -102,7 +104,7 @@ const FortuneIsland = () => {
             Book Now
           </a>
           <a
-            href='https://waze.com' // Replace with the actual Waze URL
+            href='https://www.waze.com/live-map/directions/ph/calabarzon/nasugbu/fortune-island?to=place.ChIJNTymjPfqvTMRmZ46pf_AMUg' //Waze URL
             target='_blank'
             rel='noopener noreferrer'
             className='bg-green-500 text-white px-4 py-2 rounded-md'
@@ -110,9 +112,10 @@ const FortuneIsland = () => {
             Get Directions (Waze)
           </a>
         </div>
-        <p className='mt-[30px]'>Put the short descriptions here</p> 
+        <div className='flex w-80 justify-center items-center mt-[30px]' /*added some css*/>
+            <p className='mx-3'>Fortune Island is a hidden gem nestled in the turquoise waters of the Batangas Strait, Philippines. Located just off the coast of Nasugbu, Batangas, this enchanting island is a dream destination for adventurous souls and history enthusiasts alike. Its allure lies in the mystical combination of its ancient Greek-inspired architecture, pristine beaches, and stunning rock formations, making it a truly unique and unforgettable experience.</p> 
+        </div>
       </div>
-      
       <RagalaNavbar />
     </div>
   );
